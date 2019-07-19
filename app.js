c841358630474f307c600c8e8d35a3cfd6fa1d26
inchic <poiuy1709@naver.com>
2019-07-19 17:37:31 +0900

    inchic

--- app.js
+++ app.js
@@ -15,9 +15,27 @@ const router = express.Router();
 
 
 router.get('/', function (req, res) {
-res.sendFile(path.join(__dirname + '/index.html'));
-//__dirname : It will resolve to your project folder.
+    res.sendFile(path.join(__dirname + '/main.html'));
+    //__dirname : It will resolve to your project folder.
 });
+
+
+
+
+//인식
+router.get('/inchic', function (req, res) {
+    res.sendFile(path.join(__dirname + '/inchic/index.html'));
+    //__dirname : It will resolve to your project folder.
+});
+router.get('/inchic/notice', function (req, res) {
+    res.sendFile(path.join(__dirname + '/inchic/notice.html'));
+    //__dirname : It will resolve to your project folder.
+});
+
+
+
+
+
 
 
 
